4ffe5941894bb0c311c199b5759a468f7d016014
Jorge Herrera <102686717+GhostJL@users.noreply.github.com>
2025-05-31 12:00:34 -0600

    First Commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final AppRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    )
+  ],
+);
