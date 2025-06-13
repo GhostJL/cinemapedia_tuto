e5feabf7c9fc59dd20560fa052ca15f96913218c
Jorge Herrera <102686717+GhostJL@users.noreply.github.com>
2025-06-12 23:29:28 -0600

    seccion de carga de pantalla y pantalla principal finalizada

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -38,11 +38,15 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final upComingMovies = ref.watch(upComingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
+
     return CustomScrollView(
       slivers: [
         const SliverAppBar(
